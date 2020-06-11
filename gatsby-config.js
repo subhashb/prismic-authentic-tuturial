3185f3dfc2b3471904ab0e64b910f1a00582940e
Subhash Bhushan <subhash.bhushan@gmail.com>
2020-06-11 15:57:56 -0700

    Create and configure gatsby prismic plugin

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: "Gatsby + Prismic Tutorial",
+    description: "Learn how to integrate Prismic into your Gatsby project.",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          {
+            family: "Lato",
+            variants: ["400", "400i", "700", "700i", "900"],
+          },
+          {
+            family: "Amiri",
+            variants: ["400", "400i", "700", "700i"],
+          },
+        ],
+      },
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
+    {
+      resolve: "gatsby-source-prismic-graphql",
+      options: {
+        repositoryName: "prismic-authentic-tutorial",
+      },
+    },
+  ],
+};
